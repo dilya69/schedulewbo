658698fcbffcaeb34d4389001ab43626f80a92d1
dilya69 <dilyadistik@gmail.com>
2026-09-04 09:19:14 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,4 +4,5 @@ const CONFIG = {
   SUPABASE_ANON_KEY: "sb_publishable_wqyzuo111QCFxs1oauXK9g_Fzd-oLg5",
   // адрес Edge Function telegram-auth (тот же project ref)
   AUTH_FUNCTION_URL: "https://gwkurcwggttztchmhvzy.functions.supabase.co/telegram-auth",
+   SEND_FILE_FUNCTION_URL: "https://gwkurcwggttztchmhvzy.functions.supabase.co/send-file",
 };
